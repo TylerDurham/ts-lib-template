82e9451dc98de04a6f2580e2516fb9e5eb808308
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-06-30 20:32:34 -0500

    Removed package.json reference.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import config from './common/config';
 import * as colors from 'ansi-colors';
-import * as pkg from '../package.json';
 
 const sayHello = (name: string) => {
     return `${colors.magentaBright.bold('Hello')}, ${colors.bgBlueBright.bold(name)}!`
 }
-
-console.log("\n" + sayHello(pkg.name) + "\n");
 
 export { sayHello }
